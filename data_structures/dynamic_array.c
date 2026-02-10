215da6aede7730bce1399f3acff73d5011b23db8
VulcanWM <logmedha09@gmail.com>
2026-02-10 20:23:44 +0000

    added `unload` and `display` functions

--- data_structures/dynamic_array.c
+++ data_structures/dynamic_array.c
@@ -7,36 +7,40 @@ typedef struct {
     int score;
 } leaderboard;
 
-int load(); // load from a text file into memory
-void unload(); // unload all the memory
+leaderboard *load(int *size); // load from a text file into memory
+void unload(leaderboard *ptr); // unload all the memory
 void add_data(); // add data to the dynamic array
 void remove_data(); // remove data from the dynamic array
 void amend_data(); // amend a score of a user
-void display(); // display all the scores
+void display(leaderboard *ptr, int size); // display all the scores
 void search_data(); // search for the score of a user
 void sort_data(); // sort all the score data based on score
 
 int main() {
-    // leaderboard leader = {"user1", 50};
-    // printf("%s\n", leader.username);
-    // printf("%d\n", leader.score);
-    // printf("%zo\n", sizeof(leaderboard));
-    load();
+    int size = 0;
+    leaderboard *users = load(&size);
+    if (users == NULL)
+    {
+        printf("loading failed\n");
+        return 1;
+    }
+    printf("loaded everything\n");
+    display(users, size);
+    unload(users);
+    printf("unloaded everything\n");
     return 0;
 }
 
-int load() {
+leaderboard *load(int *size) {
     FILE *source = fopen("/Users/vulcanwm/CLionProjects/memoryexplorer/data.txt", "r");
     if (source == NULL) {
         printf("source not found");
-        return 1;
+        return NULL;
     }
     char line[100];
     leaderboard *ptr = NULL;
-    int size = 0;
 
     while (fgets(line, 100, source)) {
-        size++;
         char username[50];
         char scoreStr[50];
         int scoreData = 0;
@@ -57,30 +61,39 @@ int load() {
         }
         scoreStr[i-scoreStartIndex] = '\0';
         int score = atoi(scoreStr);
-        printf("username: %s\n", username);
-        printf("score: %i\n", score);
 
         if (ptr == NULL) {
-            ptr = malloc(sizeof(leaderboard) * size);
+            ptr = malloc(sizeof(leaderboard) * (*size + 1));
             if (ptr == NULL) {
                 printf("memory allocation failed");
-                size--;
-                return 1;
+                fclose(source);
+                return NULL;
             }
         } else {
-            leaderboard *temp = realloc(ptr, sizeof(leaderboard) * size);
-            if (temp == NULL)
-            {
+            leaderboard *temp = realloc(ptr, sizeof(leaderboard) * (*size + 1));
+            if (temp == NULL) {
                 printf("memory allocation failed");
-                size--;
-                return 1;
+                free(ptr);
+                fclose(source);
+                return NULL;
             }
             ptr = temp;
         }
-        ptr[size-1].score = score;
-        strcpy(ptr[size-1].username, username);
+        ptr[*size].score = score;
+        strcpy(ptr[*size].username, username);
+        *size += 1;
     }
 
     fclose(source);
-    return 0;
+    return ptr;
+}
+
+void unload(leaderboard *ptr) {
+    free(ptr);
+}
+
+void display(leaderboard *ptr, const int size) {
+    for (int i = 0; i < size; i++) {
+        printf("username: %s, score: %d\n", ptr[i].username, ptr[i].score);
+    }
 }
