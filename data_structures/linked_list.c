76a32e01410ed4a220e9d9e2b75a7f5e52fbb785
VulcanWM <logmedha09@gmail.com>
2026-02-17 18:17:07 +0000

    started linked list, added loading, unloading and displaying

---
+++ data_structures/linked_list.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct leaderboard {
+    char username[50];
+    int score;
+    struct leaderboard *next;
+} leaderboard;
+
+leaderboard *load();
+void unload(leaderboard *users);
+void add_data();
+void remove_data();
+void amend_data();
+void display(leaderboard *users);
+void search_data();
+
+int main() {
+    leaderboard *users = load();
+    display(users);
+    unload(users);
+    return 0;
+}
+
+leaderboard *load() {
+    // loads the data from a text file and stores it in a dynamic array
+    // returns the pointer to the dynamic array
+    FILE *source = fopen("/Users/vulcanwm/CLionProjects/memoryexplorer/data.txt", "r");
+    if (source == NULL) {
+        printf("source not found");
+        return NULL;
+    }
+    char line[100];
+    leaderboard *ptr = NULL;
+
+    while (fgets(line, 100, source)) {
+        char username[50];
+        int score;
+        sscanf(line, "%49[^,],%d", username, &score);
+
+        leaderboard *newPtr = malloc(sizeof(leaderboard));
+        if (newPtr == NULL) {
+            printf("memory allocation failed");
+            return NULL;
+        }
+        strcpy(newPtr->username, username);
+        newPtr->score = score;
+        if (ptr == NULL) {
+            ptr = newPtr;
+        } else {
+            leaderboard *bPointer = ptr->next;
+            ptr->next = newPtr;
+            newPtr->next = bPointer;
+        }
+    }
+    fclose(source);
+    return ptr;
+}
+
+void unload(leaderboard *users) {
+    while (users != NULL) {
+        leaderboard *nextPtr = users->next;
+        free(users);
+        users = nextPtr;
+    }
+}
+
+void display(leaderboard *users) {
+    while (users != NULL) {
+        printf("username: %s, score: %d\n", users->username, users->score);
+        users = users->next;
+    }
+}
